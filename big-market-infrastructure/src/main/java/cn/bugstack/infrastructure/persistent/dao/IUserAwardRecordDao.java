54ac5a36fbb3ffbb196635d25855fbca4790ae1a
Asule <distinctofwang@gmail.com>
2026-09-08 19:00:41 +0800

    写入中奖记录以及任务重试补偿MQ发消息失败的情况

--- big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IUserAwardRecordDao.java
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IUserAwardRecordDao.java
@@ -1,9 +1,13 @@
 package cn.bugstack.infrastructure.persistent.dao;
 
+import cn.bugstack.infrastructure.persistent.po.UserAwardRecord;
+
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
  * @description 用户中奖记录表
  * @create 2024-04-03 15:57
  */
 public interface IUserAwardRecordDao {
+
+    void insert(UserAwardRecord userAwardRecord);
 }
